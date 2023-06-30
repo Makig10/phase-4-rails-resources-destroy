05a30735f64b0b0880638f4877a30e3da7f85994
Brian <mahuibrian14@gmail.com>
2023-06-30 14:20:47 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  #resources :birds, only: [:index, :show, :create, :update, :destroy]
+  resources :birds  #since we r using all five RESTful conventions...
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
